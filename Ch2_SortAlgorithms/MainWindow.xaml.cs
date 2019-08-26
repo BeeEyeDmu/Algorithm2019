6d8fb56e12f7dbe9ea22007ca8057a134023b009
byungik kang <bikang@hotmail.co.kr>
2019-08-26 13:49:04 +0900

    Initial commit

---
+++ Ch2_SortAlgorithms/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch2_SortAlgorithms
+{
+  /// <summary>
+  /// 10만개의 정렬: 65초와 31ms 2천배
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    int[] a = new int[100000000]; // 1억개
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void BtnRandom_Click(object sender, RoutedEventArgs e)
+    {
+      Random r = new Random();
+      for (int i = 0; i < int.Parse(txtNo.Text); i++)
+        a[i] = r.Next();
+
+      PrintArray(a);
+    }
+
+    // N*N 정렬알고리즘(버블정렬)
+    private void BtnQuad_Click(object sender, RoutedEventArgs e)
+    {
+      var watch = System.Diagnostics.Stopwatch.StartNew();
+
+      for (int i = int.Parse(txtNo.Text) - 1; i > 0; i--)
+      {
+        for (int j = 0; j < i; j++)
+        {
+          if (a[j] > a[j + 1])
+          {
+            int t = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = t;
+          }
+        }
+      }
+      watch.Stop();
+      tbQuad.Text = watch.ElapsedMilliseconds + "ms";
+      PrintArray(a);
+    }
+
+    // NlnN 알고리즘(퀵정렬)
+    private static void Quick_Sort(int[] arr, int left, int right)
+    {
+      if (left < right)
+      {
+        int pivot = Partition(arr, left, right);
+
+        if (pivot > 1)
+        {
+          Quick_Sort(arr, left, pivot - 1);
+        }
+        if (pivot + 1 < right)
+        {
+          Quick_Sort(arr, pivot + 1, right);
+        }
+      }
+    }
+
+    private static int Partition(int[] arr, int left, int right)
+    {
+      int pivot = arr[left];
+      while (true)
+      {
+        while (arr[left] < pivot)
+        {
+          left++;
+        }
+
+        while (arr[right] > pivot)
+        {
+          right--;
+        }
+
+        if (left < right)
+        {
+          if (arr[left] == arr[right])
+            return right;
+
+          int temp = arr[left];
+          arr[left] = arr[right];
+          arr[right] = temp;
+        }
+        else
+        {
+          return right;
+        }
+      }
+    }
+
+    private void PrintArray(int[] a)
+    {
+      for (int i = 0; i < int.Parse(txtNo.Text); i++)
+        Console.WriteLine(a[i]);
+    }
+
+    private void BtnLog_Click(object sender, RoutedEventArgs e)
+    {
+      var watch = System.Diagnostics.Stopwatch.StartNew();
+
+      Quick_Sort(a, 0, int.Parse(txtNo.Text)-1);
+
+      watch.Stop();
+      tbLog.Text = watch.ElapsedMilliseconds + "ms";
+      PrintArray(a);
+    }
+  }
+}
---
+++ Ch3_ClosestPair/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ch3_ClosestPair
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    Point[] points = new Point[P];
+    const int P = 10;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      //Console.WriteLine(canvas1.Width + ", " + canvas1.Height);
+
+    }
+
+    private void MakePointArray()
+    {
+      Random r = new Random();
+
+      //foreach(var p in points)
+      for (int i = 0; i < P; i++)
+      {
+        points[i].X = r.Next(400);
+        points[i].Y = r.Next(400);
+      }
+
+      foreach (var p in points)
+      {
+        Rectangle rect = new Rectangle();
+        rect.Width = 3;
+        rect.Height = 3;
+        rect.Stroke = Brushes.Black;
+        Canvas.SetLeft(rect, p.X - 1);
+        Canvas.SetTop(rect, p.Y - 1);
+        canvas1.Children.Add(rect);
+      }
+    }
+
+    private void BtnCreate_Click(object sender, RoutedEventArgs e)
+    {
+      canvas1.Children.Clear();
+      MakePointArray();
+    }
+
+    Point p1, p2;
+    int minI, minJ;
+
+    private void BtnFind_Click(object sender, RoutedEventArgs e)
+    {
+      // Brute Force
+      BruteForce();
+    }
+
+    private void BruteForce()
+    {
+      double min = double.MaxValue;
+      for (int i = 0; i < P; i++)
+      {
+        for (int j = i + 1; j < P; j++)
+        {
+          double d = Dist(i, j);
+          if (d < min)
+          {
+            min = d;
+            p1 = points[i];
+            p2 = points[j];
+            minI = i;
+            minJ = j;
+          }
+        }
+      }
+      HighLight(p1, p2);
+    }
+
+    private void HighLight(Point p1, Point p2)
+    {
+      Line line = new Line();
+      line.X1 = p1.X;
+      line.Y1 = p1.Y;
+      line.X2 = p2.X;
+      line.Y2 = p2.Y;
+      line.Stroke = Brushes.Red;
+      line.StrokeThickness = 2;
+      canvas1.Children.Add(line);
+      MessageBox.Show(String.Format("({0},{1})-({2},{3})", line.X1, line.Y1, line.X2, line.Y2));
+    }
+
+    private double Dist(int i, int j)
+    {
+      return Math.Sqrt((points[i].X - points[j].X) * (points[i].X - points[j].X) +
+        (points[i].Y - points[j].Y) * (points[i].Y - points[j].Y));
+    }
+  }
+}
